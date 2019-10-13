b816d931c8b1c0c78e56b6fdf86bb5005af62829
Anthony Hill <antdavhill@gmail.com>
2019-10-13 13:57:30 -0500

    weather API has a no cors policy

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const apiUrlEvents = '';
 const apiKeyEvents = '';
-const apiUrlWeather = '';
-const apiKeyWeather = '';
+const apiUrlWeather = 'https://www.amdoren.com/api/weather.php';
+const apiKeyWeather = 'JGvtUEzvrEiNXHqBhzdHUFbg2LDSa2';
 const apiUrlGeocode = 'https://api.opencagedata.com/geocode/v1/json';
 const apiKeyGeocode = '03e22bad1050401d963f67eaf05c9f6a';
 
@@ -23,7 +23,7 @@ function watchForm() {
 
 function getLongLat(city, date) {
     //this function formats the user info for the fetch function for the getLongLat API 
-    // then passes the formatted URL through the fetch functions for getWeather and getEvents
+    // then passes the reponse from the api through the fetch functions for getWeather and getEvents
     let encoded = encodeURI(city);
     let formatted = encoded.replace(',', '%2C')
     console.log(formatted)
@@ -33,9 +33,13 @@ function getLongLat(city, date) {
         language: 'en',
         pretty: 1
     }
+
+    //formatting the string to correctly call the API
     let queryString = Object.keys(params).map(param => `${param}=${params[param]}`).join('&');
     console.log(queryString);
     const geocodeUrl = apiUrlGeocode + '?' + queryString
+
+    //fetch function
     fetch(geocodeUrl)
     .then(response => {
       if (response.ok) {
@@ -43,13 +47,15 @@ function getLongLat(city, date) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => getWeather(responseJson.results[0].geometry, date))
-    //.then(responseJson => getEvents(responseJson.results[0].geometry, date))
+    .then(responseJson => { //passing the values through the weather and events api
+        getWeather(responseJson.results[0].geometry, date)
+        getEvents(responseJson.results[0].geometry, date)
+    })
     .catch(err => {console.log(`Something went wrong: ${err.message}`);
-
-//     getEvents(formatted);
-//     getWeather(formatted);
  });}
+
+
+
 
 function getEvents(latLng, date) {
     //this function passes the query function through on the fetch function and handles the reponse to then
@@ -58,13 +64,35 @@ function getEvents(latLng, date) {
     console.log(`getEvents date is ${date}`);
      //displayResults(responseJson);
  }
-function getWeather(latLng, date) {
-    //this function passes the correct url to combine with the weather api endpoint url to 
-   //display the weather in the DOM
-    console.log(`getWeather latLng is ${latLng}`);
-    console.log(`getWeather date is ${date}`);
 
- }
+
+
+// function getWeather(latLng, date) {
+//     //this function passes the correct url to combine with the weather api endpoint url to 
+//    //display the weather in the DOM
+//     console.log(latLng);
+//     console.log(`getWeather date is ${date}`);
+//     let eventLat = latLng.lat;
+//     let eventLon = latLng.lng;
+//     let weatherParams = {
+//         api_key: apiKeyWeather,
+//         lat: eventLat,
+//         lon: eventLon
+//     }
+//     let queryString = Object.keys(weatherParams).map(param =>`${param}=${weatherParams[param]}`).join('&');
+//     let url = apiUrlWeather + '?' + queryString;
+//     console.log(url); 
+
+//     fetch(url).then(response => {
+//         if (response.ok) {
+//           return response.json();
+//         }
+//         throw new Error(response.statusText);
+//       })
+//       ,then(responseJson => console.log(responseJson))
+//       .catch(err => {console.log(`Something went wrong getting weather: ${err.message}`);
+//       });
+//     }
 
 // function displayResults(responseJson) {
 //     //the manipulation of the DOM to show list of results
