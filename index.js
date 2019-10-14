b1b08a38402815b24cf91c1606e59fef5b9210b3
Anthony Hill <antdavhill@gmail.com>
2019-10-13 22:18:44 -0500

    took out events api and replaced with trail finder

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const apiUrlEvents = 'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events';
-const apiKeyEvents = 'F7esUapE0UVZpRyApusQPIVOlVgyUkYE';
 const apiUrlWeather = 'https://cors-anywhere.herokuapp.com/https://www.amdoren.com/api/weather.php';
 const apiKeyWeather = 'JGvtUEzvrEiNXHqBhzdHUFbg2LDSa2';
 const apiUrlGeocode = 'https://api.opencagedata.com/geocode/v1/json';
 const apiKeyGeocode = '03e22bad1050401d963f67eaf05c9f6a';
+const apiKeyTrails = '200617515-0d0fe4d295ecf7837b93fe536a715bdf'
+const apiUrlTrails = 'https://www.trailrunproject.com/data/get-trails'
 
 function watchForm() {
     //this function listens on the main page and gets the user inputted data to then pass through
@@ -18,7 +18,6 @@ function watchForm() {
     console.log(date);
     getLongLat(city, date);  
     });
-    console.log("hey now");
 }
 
 function getLongLat(city, date) {
@@ -49,31 +48,23 @@ function getLongLat(city, date) {
     })
     .then(responseJson => { //passing the values through the weather and events api
         getWeather(responseJson.results[0].geometry, date)
-        getEvents(responseJson.results[0].geometry, date)
+        getTrails(responseJson.results[0].geometry.lat, responseJson.results[0].geometry.lng)
     })
     .catch(err => {console.log(`Something went wrong: ${err.message}`);
  });}
 
 
 
-
-function getEvents(latLng, date) {
-    //this function passes the query function through on the fetch function and handles the reponse to then
-   //display results.
-    console.log(`getEvents latLng = ${latLng}`);
-    console.log(`getEvents date is ${date}`);
-    let correctLatLon = Object.keys(latLng).map(value => `${latLng[value]}`).join(',');
-    let correctDate = date + 'T00:00:00Z' //adding formatting for the API
+function getTrails(lati, lng) {
+    console.log(lati);
+    console.log(lng);
     let params = {
-        apikey: apiKeyEvents,
-        latlong: correctLatLon,
-        startDateTime: correctDate
+        key: apiKeyTrails,
+        lat: lati,
+        lon: lng
     }
-    let queryString = Object.keys(params).map(param =>
-        `${param}=${params[param]}`).join('&');
-        let url = apiUrlEvents + '?' + queryString;
-        console.log(`the url for getEvents is ${url}`)
-
+    let queryString = Object.keys(params).map(param => `${param}=${params[param]}`).join('&');
+    let url = apiUrlTrails + '?' + queryString;
     fetch(url)
     .then(response => {
         if (response.ok) {
@@ -81,10 +72,10 @@ function getEvents(latLng, date) {
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson));
- }
-
-
+    .then(responseJson => {
+        displayResults(responseJson);
+    });
+}
 
 function getWeather(latLng, date) {
    //this function passes the correct url to combine with the weather api endpoint url to 
@@ -115,17 +106,13 @@ function getWeather(latLng, date) {
 
 function displayResults(responseJson) {
     //the manipulation of the DOM to show list of results
-    for (let i=0; i<11; i++) {
-        console.log(i);
-        console.log(`inside for loop ${responseJson}`)
+    for (let i=0; i<10; i++) {
     $('#js-results').append(`<li class='list-of-results'>
-    <h1 class='event-name'>${responseJson._embedded.events[i].name}</h1>
-    <p> class='event-url>${responseJson._embedded.events[i].url}</p>
-    <p> class='event-date>${responseJson._embedded.events[i].dates.localDate} ${responseJson.embedded.events[i].dates.localTime}</p>
-    `) }
-
-    listenClicks();
-}
+    <h1 class='event-name'>${responseJson.trails[i].name}</h1>
+    <p>${responseJson.trails[i].location}</p>
+    <p>Summary: ${responseJson.trails[i].summary}</p>
+    <a href='${responseJson.trails[i].url}'<p class='event-url' target='_blank'>Visit Site</p></a>
+    `) } }
 
 function displayWeather(responseJson) {
     //displays the weather in the DOM
@@ -135,14 +122,54 @@ function displayWeather(responseJson) {
         <p>Low: ${responseJson.forecast[0].min_f}</p>
         <p>High: ${responseJson.forecast[0].max_f}</p>
         <p>Summary: ${responseJson.forecast[0].summary}</p
-    </li>`)
+//     </li>`)
 }
+
+$(watchForm());
+
+
+
+
+
+
+
+
+
+// function getEvents(latLng, date) {
+//     //this function passes the query function through on the fetch function and handles the reponse to then
+//    //display results.
+//     console.log(`getEvents latLng = ${latLng}`);
+//     console.log(`getEvents date is ${date}`);
+//     let correctLatLon = Object.keys(latLng).map(value => `${latLng[value]}`).join(',');
+//     let correctDate = date + 'T00:00:00Z' //adding formatting for the API
+//     let endDate = date + 'T23:59:59Z';
+//     let params = {
+//         apikey: apiKeyEvents,
+//         latlong: correctLatLon,
+//         startDateTime: correctDate,
+//         endDateTime: endDate,
+//         radius: 50
+//     }
+//     let queryString = Object.keys(params).map(param =>
+//         `${param}=${params[param]}`).join('&');
+//         let url = apiUrlEvents + '?' + queryString;
+//         console.log(`the url for getEvents is ${url}`)
+
+//     fetch(url)
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         }
+//         throw new Error(response.statusText);
+//     })
+//     .then(responseJson => displayResults(responseJson));
+//  }
+
+
+
 
 // STRETCH GOAL
 // function listenClicks() {
 //     //function listens for clicks on the description button and runs function
 //     getDescription
 // // 
-
-$(watchForm());
-
